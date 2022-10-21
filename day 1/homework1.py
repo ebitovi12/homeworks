410fd89b3dfb6ce908595e224a2b5acd7516b5dd
mziko ebitovi <khvicha.ebitovi966@ens.tsu.ge>
2022-10-21 22:18:27 +0400

    day1 code comit

---
+++ day 1/homework1.py
@@ -0,0 +1,10 @@
+name="khvicha" #str
+surname="ebitovi" #str
+age=21 #int
+height=1.67 #float
+knows_programing=False #bool
+is_motivated=True#book
+salary="500" #int
+
+print(name+" "+surname+" "+ str(age)+" "+str(height)+" "+str(knows_programing)+" "
++str(is_motivated)+" "+str(salary))
