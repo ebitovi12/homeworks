17ca692e0f509e856bce0b06af88da7d517e782a
mziko ebitovi <khvicha.ebitovi966@ens.tsu.ge>
2022-10-21 23:45:14 +0400

    day2 code commit

--- day2/.vs/lesson2.py
+++ day2/.vs/lesson2.py
@@ -1,1 +1,33 @@
-print("this is a lesson 2")
+# name="nika"
+# print(len(name)) #len სიგრძე
+# 4
+# my_setnence="""გამარჯობა,შეგახსენებთ 
+# რომ ხვალ გაქვთ ლექცია"""
+# print(my_setnence)
+# knows_programing=True
+# # print("hello")
+# #tab ის საშუალებით მოვახდინეთ ინდენტაცია
+# full_name="khvicha ebitovi"
+# print(len(full_name)) # პასუხია 15 თუმცა 1 სფეისის არის 
+# ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+# print(full_name[0]) #[0]ვბეჭდავთ პოზიციას
+# #position - index სინონიმებია
+# print(full_name[8])
+# print("k" not in full_name)
+# full_name="khvicha ebitovi"
+# print(full_name[2:9:3])
+#  #start-finish-step
+# print(full_name[8:])
+# #4dan bolomde
+# # print(full_name[:6])
+# #full_name = " khvicha ebitovi "
+# # print(full_name[-1:-4:-1])
+
+# #სტრინგს აქვს მეთოდები
+# #str.upper()
+# #len(str)
+#  #print(full_name.upper())
+#  name="kvicha"
+# print(name.lower())
+# name = "      khvicha "
+# print(name.replace("a","#"))
